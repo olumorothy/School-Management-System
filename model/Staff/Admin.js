e0ed8af5cd7fe919e2e379ee09c0b7e3e532cc3c
Olumorothy <iseoluwa89@gmail.com>
2023-04-04 14:45:09 +0100

    implements hashing for admin passowrd

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const adminSchema = new mongoose.Schema(
   {
     name: {
@@ -20,6 +21,18 @@ const adminSchema = new mongoose.Schema(
   },
   { timeStamp: true }
 );
+
+//Hashing user password
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.genPassword(this.password, salt);
+  next();
+});
+
 const Admin = mongoose.model("Admin", adminSchema);
 
 module.exports = Admin;
